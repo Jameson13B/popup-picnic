2197f26249ec4ffbd4596e977678f7c0f80e452a
Jameson13B <jamesonbrown@neolivity.com>
2019-03-14 09:58:19 -0600

    Added axios and connected to hosted backend to pull in data and display summary

---
+++ src/components/misc/PicnicSum.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="picnic-sum">
+    <h2>{{ picnic.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PicnicSum",
+  props: ["picnic"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="schedule">
     <h2>Schedule</h2>
+    <ul class="schedule-list">
+      <PicnicSum v-for="picnic in picnics" :picnic="picnic" :key="picnic._id" />
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import PicnicSum from "@/components/misc/PicnicSum";
+
 export default {
   name: "Schedule",
   data() {
-    return {};
+    return {
+      picnics: []
+    };
+  },
+  components: {
+    PicnicSum
+  },
+  created() {
+    axios
+      .get("https://popup-picnic-server.herokuapp.com/picnics")
+      .then(res => {
+        this.picnics = res.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 };
 </script>
